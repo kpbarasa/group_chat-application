fa7e4f212fe1ff2b8d85d5368f7bd35337bffdf7
kpbarasa <65596472+kpbarasa@users.noreply.github.com>
2022-09-30 08:36:26 +0300

    final commit

--- app.js
+++ app.js
@@ -25,9 +25,6 @@ app.use(require('express-session')({
     maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
   },
   store: store,
-  // Boilerplate options, see:
-  // * https://www.npmjs.com/package/express-session#resave
-  // * https://www.npmjs.com/package/express-session#saveuninitialized
   resave: true,
   saveUninitialized: true
 }));
